20f7b8747d9afad0495ad889c78195c890fbb647
mjd10m <mjd10m@outlook.com>
2022-08-02 22:12:36 -0400

    html file created

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const http = require('http');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach((profileitem) => console.log(profileitem))
-};
+const [name1, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name1, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see output');
+})
+
