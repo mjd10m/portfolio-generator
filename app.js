f46bcbae0264069ca4b6d257f82b2397b702d354
mjd10m <mjd10m@outlook.com>
2022-08-04 17:52:58 -0400

    inquirer setup

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const fs = require('fs');
-const http = require('http');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
 
-const [name1, github] = profileDataArgs;
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const http = require('http');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('index.html', generatePage(name1, github), err => {
-    if(err) throw err;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-    console.log('Portfolio complete! Check out index.html to see output');
-})
+// const [name1, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name1, github), err => {
+    // if(err) throw err;
+
+    // console.log('Portfolio complete! Check out index.html to see output');
+// });
 
